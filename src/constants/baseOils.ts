5da746c548e7e032ab3deb487bd542562040e9aa
afiq-syazwan192 <afiq.syazwan192@gmail.com>
2025-04-08 23:24:25 +0800

    Remove node_modules and add proper .gitignore

--- src/constants/baseOils.ts
+++
@@ -1,5 +0,0 @@
-export const BASE_OILS = [
-    { type: 'Canola Oil', specificGravity: 0.930 },
-    { type: 'Mineral Oil', specificGravity: 0.850 },
-    { type: 'Synthetic Oil', specificGravity: 0.900 }
-]; 
